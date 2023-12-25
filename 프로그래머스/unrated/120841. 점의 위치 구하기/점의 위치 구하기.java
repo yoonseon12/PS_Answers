b9a49e23ee6f7712200280793870bcd750980ce8
이윤선 <yoonseon3@gmail.com>
2023-12-25 22:57:42 +0900

    [level unrated] Title: 점의 위치 구하기, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/unrated/120841. 점의 위치 구하기/점의 위치 구하기.java
+++ 프로그래머스/unrated/120841. 점의 위치 구하기/점의 위치 구하기.java
@@ -1,11 +1,9 @@
 class Solution {
     public int solution(int[] dot) {
         if (dot[0] * dot[1] > 0) {
-            if (dot[0] > 0) return 1;
-            else return 3;
+            return dot[0] > 0 ? 1 : 3;
         } else {
-            if (dot[0] > 0) return 4;
-            else return 2;
+            return dot[0] > 0 ? 4 : 2;
         }
     }
 }
