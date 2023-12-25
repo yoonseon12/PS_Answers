c1885741407395a87ad140d0f66f56c77d9ba252
이윤선 <yoonseon3@gmail.com>
2023-12-25 23:58:58 +0900

    [level unrated] Title: 피자 나눠 먹기 (3), Time: 0.03 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/unrated/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
+++ 프로그래머스/unrated/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -1,12 +1,5 @@
 class Solution {
     public int solution(int slice, int n) {
-        int answer = 0;
-        for (int i=1; i<=100; i++) {
-            if (i * slice >= n) {
-                answer = i;
-                break;
-            }
-        }
-        return answer;
+        return n % slice > 0 ? n / slice + 1 : n / slice;
     }
 }
