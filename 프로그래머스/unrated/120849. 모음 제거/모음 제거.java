60e2d21470c7612e83d65b264de03ee7c927c64a
이윤선 <yoonseon3@gmail.com>
2023-12-30 22:14:37 +0900

    [level unrated] Title: 모음 제거, Time: 0.12 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/unrated/120849. 모음 제거/모음 제거.java
@@ -2,12 +2,6 @@ import java.util.List;
 
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
-        List<Character> list = List.of('a', 'e', 'i', 'o', 'u');
-        for (char c : my_string.toCharArray()) {
-            if (list.contains(c)) continue;
-            answer+=String.valueOf(c);
-        }
-        return answer;
+        return my_string.replaceAll("[aeiou]","");
     }
 }
