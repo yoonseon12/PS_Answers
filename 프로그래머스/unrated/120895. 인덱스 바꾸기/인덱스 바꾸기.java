cac18f75400ca0c2c89e22a769d1b1cb12874933
이윤선 <yoonseon3@gmail.com>
2023-12-31 03:25:09 +0900

    [level unrated] Title: 인덱스 바꾸기, Time: 1.16 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String[] arr = my_string.split("");
+        String temp = arr[num1];
+        arr[num1] = arr[num2];
+        arr[num2] = temp;
+        return Arrays.stream(arr).collect(Collectors.joining());
+    }
+}
