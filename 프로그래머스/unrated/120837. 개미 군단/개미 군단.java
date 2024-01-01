0a43b3b4b1ccf4dc89aead7372b834629296567f
이윤선 <yoonseon3@gmail.com>
2024-01-01 16:02:50 +0900

    [level unrated] Title: 개미 군단, Time: 0.03 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/unrated/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/unrated/120837. 개미 군단/개미 군단.java
@@ -1,14 +1,5 @@
 class Solution {
     public int solution(int hp) {
-        int answer = 0;
-        int[] powers = {5, 3, 1};
-        for (int power : powers) {
-            if (hp/ power != 0) {
-                answer += hp/power;
-                hp %= power;
-            }
-        }
-
-        return answer;
+        return (hp/5) + ((hp%5)/3) + (hp%5)%3;
     }
 }
