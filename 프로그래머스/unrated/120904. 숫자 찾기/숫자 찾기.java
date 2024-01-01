7f7951cf6d91b18f5919f503c84b4a0a492e945c
이윤선 <yoonseon3@gmail.com>
2024-01-01 16:23:16 +0900

    [level unrated] Title: 숫자 찾기, Time: 7.90 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/unrated/120904. 숫자 찾기/숫자 찾기.java
@@ -1,8 +1,5 @@
 class Solution {
     public int solution(int num, int k) {
-        String numToString = num+"";
-        String kToString = k+"";
-        int index = numToString.indexOf(kToString);
-        return index > -1 ? index+1 : -1;
+        return ("-" + num).indexOf(String.valueOf(k));
     }
 }
