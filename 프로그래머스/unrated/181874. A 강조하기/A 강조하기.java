569519ca23f614717120fd0df2cdf9a3a0653475
이윤선 <yoonseon3@gmail.com>
2024-01-01 22:58:53 +0900

    [level unrated] Title: A 강조하기, Time: 0.03 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.java
@@ -1,11 +1,7 @@
 class Solution {
     public String solution(String myString) {
-        String answer = "";
-        String[] str = myString.split("");
-        for (String s : str) {
-            if ("a".equals(s) || "A".equals(s)) answer += "A";
-            else answer += s.toLowerCase();
-        }
-        return answer;
+        myString = myString.toLowerCase();
+        myString = myString.replace("a", "A");
+        return myString;
     }
 }
