72763f9c0409184d0be37738beac920afdf254bc
이윤선 <yoonseon3@gmail.com>
2024-01-01 17:46:29 +0900

    [level unrated] Title: 로그인 성공?, Time: 0.02 ms, Memory: 67.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120883. 로그인 성공？/로그인 성공？.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String[] id_pw, String[][] db) {
+        String answer = "";
+        for (String[] loginInfo : db) {
+            if (Arrays.equals(id_pw, loginInfo)) {
+                answer = "login";
+            } else if (id_pw[0].equals(loginInfo[0])) {
+                answer = "wrong pw";
+                break;
+            } else { 
+                answer = "fail";
+            }
+        }
+        return answer;
+    }
+}
