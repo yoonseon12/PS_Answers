86e4f47625475e3d6f1f8ab290d5af5ab0da0be4
이윤선 <yoonseon3@gmail.com>
2024-01-10 21:25:56 +0900

    [level unrated] Title: 컨트롤 제트, Time: 0.19 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/unrated/120853. 컨트롤 제트/컨트롤 제트.java
+++ 프로그래머스/unrated/120853. 컨트롤 제트/컨트롤 제트.java
@@ -1,7 +1,6 @@
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        int z_index = -1;
         String[] arr = s.split(" ");
         for (int i=0; i<arr.length; i++) {
             if (arr[i].equals("Z")) {
