95dc4d659016f53ce927b88822dc4889f5bd2749
이윤선 <yoonseon3@gmail.com>
2024-02-25 01:43:47 +0900

    [Silver II] Title: 주식, Time: 1300 ms, Memory: 318448 KB -BaekjoonHub

---
+++ 백준/Silver/11501. 주식/주식.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for (int i=0; i<T; i++) {
+			int N = Integer.parseInt(br.readLine());
+			int[] arr = new int[N];
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j=0; j<N; j++) {
+				arr[j] = Integer.parseInt(st.nextToken());
+			}
+
+			long result = 0;
+			int max = 0;
+			for (int j=N-1; j>=0; j--) {
+				max = Math.max(max, arr[j]);
+				if (j == N-1) continue;
+				if (max - arr[j] > 0) {
+					result += max-arr[j];
+				}
+			}
+			sb.append(result+"\n");
+		}
+		System.out.println(sb);
+	}
+}
