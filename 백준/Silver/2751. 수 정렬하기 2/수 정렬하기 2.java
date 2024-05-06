99900c2381a7d6b413c920a818a9799e2c886fc0
LEE-YOONSEON <yoonseon3@gmail.com>
2024-05-07 02:31:08 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1572 ms, Memory: 171620 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    static int N;
+    static List<Integer> list;
+
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            N = Integer.parseInt(br.readLine());
+
+            list = new ArrayList<>();
+            for (int i=0; i<N; i++) {
+                list.add(Integer.parseInt(br.readLine()));
+            }
+            Collections.sort(list);
+            
+            StringBuilder sb = new StringBuilder();
+            for(int i : list) {
+                sb.append(i+"\n");
+            }
+
+            System.out.println(sb.toString().trim());
+        }
+    }
+}
