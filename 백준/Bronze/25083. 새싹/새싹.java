b78ac9b72b7d4b1a25b22265d8b1e4dc809d7e55
LEE-YOONSEON <yoonseon3@gmail.com>
2024-08-02 04:58:51 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 13988 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String result =
+            "         ,r'\"7\n"
+            + "r`-_   ,'  ,/\n"
+            + " \\. \". L_r'\n"
+            + "   `~\\/\n"
+            + "      |\n"
+            + "      |";
+        System.out.println(result);
+    }
+}
