a55ab1c260fb95a99f4c688727a0447d36685a7b
LEE-YOONSEON <yoonseon3@gmail.com>
2024-08-02 03:44:41 +0900

    [Bronze I] Title: 단어 공부, Time: 288 ms, Memory: 21084 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -4,26 +4,30 @@ import java.io.InputStreamReader;
 
 public class Main {
 
+    static String str;
+
     public static void main(String[] args) throws IOException {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            var str = br.readLine().toLowerCase();
-            var countMap = new int[26];
-            str.chars().forEach(c -> {
-                countMap[c - 'a']++;
-            });
+            str = br.readLine().toLowerCase();
 
-            var maxCount = Integer.MIN_VALUE;
-            var maxValue = '?';
+            int[] countArr = new int[26];
+
+            for (int i=0; i<str.length(); i++) {
+                countArr[str.charAt(i) -'a'] ++;
+            }
+
+            int maxCount = Integer.MIN_VALUE;
+            char maxValue = '?';
             for (int i=0; i<26; i++) {
-                if (maxCount < countMap[i]) {
-                    maxCount = countMap[i];
+                if (countArr[i] > maxCount) {
+                    maxCount = countArr[i];
                     maxValue = (char)(i + 'A');
-                } else if (countMap[i] == maxCount) {
+                } else if (maxCount == countArr[i]) {
                     maxValue = '?';
                 }
             }
 
-            System.out.println(maxValue == '?' ? "?" : maxValue);
+            System.out.println(maxValue);
         }
     }
 }
