9ebbeef22cfb0c5014a0fcd98864aefc55262b26
LEE-YOONSEON <yoonseon3@gmail.com>
2024-08-03 04:30:25 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 796 ms, Memory: 239888 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try (
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        ) {
+            int N = Integer.parseInt(br.readLine());
+
+            for (int i = 0; i < N; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+
+                bw.write(String.valueOf(a+b));
+                bw.newLine();
+            }
+            bw.flush();
+        }
+    }
+}
