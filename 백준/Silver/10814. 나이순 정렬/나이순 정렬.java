b14b03bed7971f25c82db8f891f66beeb350289b
LEE-YOONSEON <yoonseon3@gmail.com>
2024-08-05 00:06:15 +0900

    [Silver V] Title: 나이순 정렬, Time: 1320 ms, Memory: 59808 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N;
+    static String[][] persons;
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            N = Integer.parseInt(br.readLine());
+            persons = new String[N][2];
+            for (int i = 0; i < N; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                persons[i][0] = st.nextToken();
+                persons[i][1] = st.nextToken();
+            }
+
+            Arrays.sort(persons, (p1, p2) -> {
+                if (p1[0].equals(p2[0])) {
+                    return 0;
+                }
+                return Integer.parseInt(p1[0]) - Integer.parseInt(p2[0]);
+            });
+
+            for (String[] person : persons) {
+                System.out.println(person[0] + " " + person[1]);
+            }
+        }
+    }
+}
